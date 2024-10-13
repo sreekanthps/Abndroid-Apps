e681fa6b572bf9a39b23f5c7c5eff88c8669065c
sreekanth Pulichjerla <sreekanth.pulicherla@gmail.com>
2024-10-13 18:09:37 +0800

    Simple Android Toast roject

---
+++ app/src/main/java/com/example/simpletoast/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.simpletoast;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText myeditText;
+    Button mybutton;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        myeditText = findViewById(R.id.mytextField);
+        mybutton = findViewById(R.id.mybutton);
+        mybutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String inputName = myeditText.getText().toString();
+                Toast.makeText(MainActivity.this,"Welcome " + inputName + " to the new toast",Toast.LENGTH_LONG);
+            }
+        });
+    }
+}
